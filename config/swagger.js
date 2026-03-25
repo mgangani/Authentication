a10a94502c351461a9cfb2d0cdcce0c97d5e7e21
mgangani <mgangani@codewinglet.com>
2026-03-25 12:00:24 +0530

    fix: update local server URL to port 6000 in Swagger and password reset email

--- config/swagger.js
+++ config/swagger.js
@@ -10,7 +10,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "http://localhost:3000",
+      url: "http://localhost:6000",
       description: "Local development server",
     },
   ],
