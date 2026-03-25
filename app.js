95cf95f4e89a7b78cb954396643ca1abb048e90b
mgangani <mgangani@codewinglet.com>
2026-03-25 11:39:07 +0530

    chore: update server port from 3000 to 6000 for application

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const app = express();
 const start = async () => {
   await connectDB();
 
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  app.listen(6000, () => {
+    console.log("Server is running on port 6000");
   });
 };
 
